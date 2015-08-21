da443565b9bb5d18271135b0447b4fee4ccc8df6
hvdwolf <hvdwolf@gmail.com>
2015-08-21 18:42:06 +0200

    increase negative id

--- exportAddress.sql
+++ exportAddress.sql
@@ -3,7 +3,7 @@
 .echo on
 
 
--- Export to POI file as txt file to workfiles/<Country>_address_europe.txt
+-- Export as OSM structured txt file to workfiles/<Country>_address_europe.txt
 
 .echo off
 
@@ -14,56 +14,56 @@
 -- the standard to describe your entries are not yet in OSM (yet).
 -- England
 .output workfiles/England_postcodes_address_europe.txt
-select '<node id="-' || ROWID || '" lon="' || mpc.longitude || '" lat="' || mpc.latitude || '" visible="true">',
+select '<node id="-' || (ROWID + 1000000) || '" lon="' || mpc.longitude || '" lat="' || mpc.latitude || '" visible="true">',
  '<tag k="addr:postcode" v="' || mpc.postcode || '"/>', '<tag k="building" v="yes"/>',
  '<tag k="addr:housenumber" v="' || mpc.postcode || '"/>',
  '<tag k="addr:city" v="' || mpc.city || '"/>', '<tag k="addr:country" v="' || mpc.IsoCountry || '"/> </node>'
  from mypostcodes mpc where city not null and country='England';
 
  -- Now select all postcodes that don't have a city (entry in postcodes but no entry in ukpostcodes)
- select '<node id="-' || ROWID || '" lon="' || mpc.longitude || '" lat="' || mpc.latitude || '" visible="true">',
+  select '<node id="-' || (ROWID + 1000000) || '" lon="' || mpc.longitude || '" lat="' || mpc.latitude || '" visible="true">',
  '<tag k="addr:postcode" v="' || mpc.postcode || '"/>', '<tag k="building" v="yes"/>',
  '<tag k="addr:housenumber" v="' || mpc.postcode || '"/>', '<tag k="addr:country" v="' || mpc.IsoCountry || '"/> </node>'
  from mypostcodes mpc where city is null  and country='England';
  
  -- Northern-Ireland
 .output workfiles/Northern-Ireland_postcodes_address_europe.txt
-select '<node id="-' || ROWID || '" lon="' || mpc.longitude || '" lat="' || mpc.latitude || '" visible="true">',
+select '<node id="-' || (ROWID + 1000000) || '" lon="' || mpc.longitude || '" lat="' || mpc.latitude || '" visible="true">',
  '<tag k="addr:postcode" v="' || mpc.postcode || '"/>', '<tag k="building" v="yes"/>',
  '<tag k="addr:housenumber" v="' || mpc.postcode || '"/>',
  '<tag k="addr:city" v="' || mpc.city || '"/>', '<tag k="addr:country" v="' || mpc.IsoCountry || '"/> </node>'
  from mypostcodes mpc where city not null and country='Northern Ireland';
 
  -- Now select all postcodes that don't have a city (entry in postcodes but no entry in ukpostcodes)
- select '<node id="-' || ROWID || '" lon="' || mpc.longitude || '" lat="' || mpc.latitude || '" visible="true">',
+ select '<node id="-' || (ROWID + 1000000) || '" lon="' || mpc.longitude || '" lat="' || mpc.latitude || '" visible="true">',
  '<tag k="addr:postcode" v="' || mpc.postcode || '"/>', '<tag k="building" v="yes"/>',
  '<tag k="addr:housenumber" v="' || mpc.postcode || '"/>', '<tag k="addr:country" v="' || mpc.IsoCountry || '"/> </node>'
  from mypostcodes mpc where city is null  and country='Northern Ireland';
  
  -- Scotland
 .output workfiles/Scotland_postcodes_address_europe.txt
-select '<node id="-' || ROWID || '" lon="' || mpc.longitude || '" lat="' || mpc.latitude || '" visible="true">',
+select '<node id="-' || (ROWID + 1000000) || '" lon="' || mpc.longitude || '" lat="' || mpc.latitude || '" visible="true">',
  '<tag k="addr:postcode" v="' || mpc.postcode || '"/>', '<tag k="building" v="yes"/>',
  '<tag k="addr:housenumber" v="' || mpc.postcode || '"/>',
  '<tag k="addr:city" v="' || mpc.city || '"/>', '<tag k="addr:country" v="' || mpc.IsoCountry || '"/> </node>'
  from mypostcodes mpc where city not null and country='Scotland';
 
  -- Now select all postcodes that don't have a city (entry in postcodes but no entry in ukpostcodes)
- select '<node id="-' || ROWID || '" lon="' || mpc.longitude || '" lat="' || mpc.latitude || '" visible="true">',
+ select '<node id="-' || (ROWID + 1000000) || '" lon="' || mpc.longitude || '" lat="' || mpc.latitude || '" visible="true">',
  '<tag k="addr:postcode" v="' || mpc.postcode || '"/>', '<tag k="building" v="yes"/>',
  '<tag k="addr:housenumber" v="' || mpc.postcode || '"/>', '<tag k="addr:country" v="' || mpc.IsoCountry || '"/> </node>'
  from mypostcodes mpc where city is null  and country='Scotland';
  
 -- Wales
 .output workfiles/Wales_postcodes_address_europe.txt
-select '<node id="-' || ROWID || '" lon="' || mpc.longitude || '" lat="' || mpc.latitude || '" visible="true">',
+select '<node id="-' || (ROWID + 1000000) || '" lon="' || mpc.longitude || '" lat="' || mpc.latitude || '" visible="true">',
  '<tag k="addr:postcode" v="' || mpc.postcode || '"/>', '<tag k="building" v="yes"/>',
  '<tag k="addr:housenumber" v="' || mpc.postcode || '"/>',
  '<tag k="addr:city" v="' || mpc.city || '"/>', '<tag k="addr:country" v="' || mpc.IsoCountry || '"/> </node>'
  from mypostcodes mpc where city not null and country='Wales';
 
  -- Now select all postcodes that don't have a city (entry in postcodes but no entry in ukpostcodes)
- select '<node id="-' || ROWID || '" lon="' || mpc.longitude || '" lat="' || mpc.latitude || '" visible="true">',
+ select '<node id="-' || (ROWID + 1000000) || '" lon="' || mpc.longitude || '" lat="' || mpc.latitude || '" visible="true">',
  '<tag k="addr:postcode" v="' || mpc.postcode || '"/>', '<tag k="building" v="yes"/>',
  '<tag k="addr:housenumber" v="' || mpc.postcode || '"/>', '<tag k="addr:country" v="' || mpc.IsoCountry || '"/> </node>'
  from mypostcodes mpc where city is null  and country='Wales';
